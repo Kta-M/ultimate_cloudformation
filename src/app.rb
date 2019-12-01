32b24353cd50187ed41c93192295aa770d04d9d0
Keita Mohri <mohri1219@gmail.com>
2019-12-02 06:45:26 +0900

    use lambda tag

--- src/app.rb
+++ src/app.rb
@@ -1,14 +1,14 @@
 require 'json'
+require 'aws-sdk-lambda'
 require 'aws-sdk-cloudformation'
 
 def lambda_handler(event:, context:)
-  # 関数名からスタック名取得
-  cfn = Aws::CloudFormation::Client.new
-  resp = cfn.describe_stack_resources({
-    physical_resource_id: ENV['FUNCTION_NAME']
-  })
-  stack_name = resp.stack_resources.first.stack_name
+  # スタック名を取得
+  lambda_client = Aws::Lambda::Client.new
+  resp = lambda_client.list_tags({resource: event["lambda_arn"]})
+  stack_name = resp.tags["aws:cloudformation:stack-name"]
 
   # スタックを削除
-  cfn.delete_stack(stack_name: stack_name)
+  cfn_client = Aws::CloudFormation::Client.new
+  cfn_client.delete_stack(stack_name: stack_name)
 end
