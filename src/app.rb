e0eceb5817f17983c0976ef80da724bd4e29ec27
Keita Mohri <mohri1219@gmail.com>
2019-12-07 19:41:01 +0900

    use AWS::StackName

--- src/app.rb
+++ src/app.rb
@@ -1,14 +1,8 @@
 require 'json'
-require 'aws-sdk-lambda'
 require 'aws-sdk-cloudformation'
 
 def lambda_handler(event:, context:)
-  # スタック名を取得
-  lambda_client = Aws::Lambda::Client.new
-  resp = lambda_client.list_tags({resource: event["lambda_arn"]})
-  stack_name = resp.tags["aws:cloudformation:stack-name"]
-
-  # スタックを削除
+  stack_name = ENV['STACK_NAME']
   cfn_client = Aws::CloudFormation::Client.new
   resp = cfn_client.describe_stacks(stack_name: stack_name)
   if resp.stacks.first.stack_status == "CREATE_COMPLETE"
