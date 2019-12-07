0369b3292b46943bc8c05f4fdf5ee9d816d39c30
Keita Mohri <mohri1219@gmail.com>
2019-12-07 19:42:11 +0900

    nest template

--- src/app.rb
+++ src/app.rb
@@ -2,10 +2,10 @@ require 'json'
 require 'aws-sdk-cloudformation'
 
 def lambda_handler(event:, context:)
-  stack_name = ENV['STACK_NAME']
-  cfn_client = Aws::CloudFormation::Client.new
-  resp = cfn_client.describe_stacks(stack_name: stack_name)
-  if resp.stacks.first.stack_status == "CREATE_COMPLETE"
-    cfn_client.delete_stack(stack_name: stack_name)
+  msg =  event['Records'].first.dig('Sns', 'Message')
+  if msg.include?("ResourceStatus='CREATE_COMPLETE'") &&
+      msg.include?("LogicalResourceId='#{ENV['CHILD_STACK_NAME']}'")
+    cfn_client = Aws::CloudFormation::Client.new
+    cfn_client.delete_stack(stack_name: ENV['PARENT_STACK_NAME'])
   end
 end
